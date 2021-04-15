1d2ef0f75fefa2524a709ff13e3d9f6163f952b0
thetnaing078 <thetnaing078106@gmail.com>
2021-04-15 23:49:59 +0630

    Update index.jsx

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,5 +1,5 @@
 import { Container, Typography, Button, Grid } from '@material-ui/core';
-import logo from './Canon-Kit.png';
+import logo from './Brand.png';
 import './style.css';
 
 const Banner = () => {
