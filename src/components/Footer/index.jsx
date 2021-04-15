60b472d0c4f9a572cc5f2bfacf2cfd2e88ca0faf
thetnaing078 <thetnaing078106@gmail.com>
2021-04-15 14:20:07 +0630

    first commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+const Footer = () => {
+	const date = new Date();
+	const fullYear = date.getFullYear();
+	return (
+		<footer className="footer">
+			<p>All &copy; copy rights are reserved to SHAN MALL {fullYear}</p>
+		</footer>
+	);
+};
+
+export default Footer;
